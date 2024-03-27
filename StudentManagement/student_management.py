20f147c4a831fce5d624929f21ce91805eabaaa6
thenghiabk <thenghiabk@gmail.com>
2024-03-27 15:06:24 +0700

    updated

--- StudentManagement/student_management.py
+++ StudentManagement/student_management.py
@@ -13,12 +13,34 @@ class StudentManagement:
     def add_student(self, student):
         self.students.append(student)
 
+    def search_student(self, name):
+        found_students = []
+        for student in self.students:
+            if student.name == name:
+                found_students.append(student)
+        return found_students
+
+    def remove_student(self, name):
+        for student in self.students:
+            if student.name == name:
+                self.students.remove(student)
+                return "Student removed successfully."
+        return "Student not found in the database."
+
 
 student_management = StudentManagement();
-print(student_management.students)
 
 student1 = Student("John", 22, 55)
+student2 = Student("David", 20, 75)
+student3 = Student("Nancy", 21, 85)
+
 student_management.add_student(student1)
-print()
+student_management.add_student(student2)
+student_management.add_student(student3)
+
+result = student_management.search_student("John")
+removed_result = student_management.remove_student("John")
+
+print(removed_result)
 
 
